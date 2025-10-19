3c6e917f6c745bb1a631e23608258973013f8de0
evgenia-fienik <160489180+evgenia-fienik@users.noreply.github.com>
2025-10-19 16:31:29 +0300

    Add swagger docs for contacts and login endpoints

--- src/server.js
+++ src/server.js
@@ -1,13 +1,19 @@
 import path from 'node:path';
+import * as fs from 'node:fs';
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
 import cookieParser from 'cookie-parser';
+import swaggerUi from 'swagger-ui-express';
 
 import routes from './routers/index.js';
 
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
+
+const SWAGGER_DOCUMENT = JSON.parse(
+  fs.readFileSync(path.join('docs', 'swagger.json')),
+);
 
 export const setupServer = () => {
   const app = express();
@@ -16,6 +22,8 @@ export const setupServer = () => {
   app.use(cors());
   app.use(pino());
   app.use(cookieParser());
+
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(SWAGGER_DOCUMENT));
 
   app.use('/photos', express.static(path.resolve('src', 'uploads', 'photos')));
 
